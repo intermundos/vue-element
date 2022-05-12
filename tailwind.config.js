555eef5bff8a335dafbb9321dbb7f218e472cf26
intermundos <intermundas@gmail.com>
2022-05-12 09:56:25 +0300

    updates tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,43 @@
 import { defineConfig } from 'windicss/helpers'
-// import colors           from 'windicss/colors'
-// import plugin           from 'windicss/plugin'
-//
-// console.log(plugin)
 
 export default defineConfig( {
   // preflight: true,
   darkMode:  'class',
-  important: true,
+  important: true, // to override element plus styles
   plugins:   [
     require( '@windicss/plugin-question-mark' ),
+    require( 'windicss/plugin/line-clamp' ),
     // require( 'windicss/plugin/filters' ),
     // require( 'windicss/plugin/forms' ),
     // require( 'windicss/plugin/aspect-ratio' ),
-    // require( 'windicss/plugin/line-clamp' ),
+    require( '@windicss/plugin-heropatterns' )( {
+      // the list of patterns you want to generate a class for
+      // the names must be in kebab-case
+      // an empty array will generate all 87 patterns
+      patterns: [ 'signal' ],
+
+      // colors: {
+      //   'default': '#ff3a64',
+      //   'blue': '#123345',
+      // },
+
+      // The foreground opacity
+      opacity: {
+        default: '0.1',
+        opac: '0.03',
+        low: '0.05',
+        10: '0.1',
+        20:     '0.2',
+        30:     '0.3',
+        40:     '0.4',
+        50:     '0.5',
+        60:     '0.6',
+        70:     '0.7',
+        80:     '0.8',
+        90:     '0.9',
+        100:     '1.0',
+      },
+    } )
   ],
   shortcuts: {
     'btn': 'py-2 px-4 font-semibold rounded-lg shadow-md',
